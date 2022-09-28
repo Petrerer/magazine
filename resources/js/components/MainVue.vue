a734175c04210dc9abe2b90ec41cb402f8c7f0c8
Piotr Franc <franc.petr1234@gmail.com>
2022-09-28 17:48:46 +0200

    added logout

--- resources/js/components/MainVue.vue
+++ resources/js/components/MainVue.vue
@@ -9,6 +9,9 @@
             <router-link to="/categories/list">
                 <v-btn class="primary white--text ma-3">Kategorie</v-btn>
             </router-link>
+
+            <v-spacer></v-spacer>
+            <v-btn class="primary white--text ma-3" @click="logout">Wyloguj się</v-btn>
             
         </v-footer>
         <v-main>
@@ -19,8 +22,14 @@
 
 <script>
     export default {
-        mounted() {
-
+        methods:{
+            async logout(){
+                await this.$http.post("/logout")
+                .then(response=>{
+                    console.log(response);
+                })
+                window.location.reload();
+            }
         }
     }
 </script>
